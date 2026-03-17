2f568fcc2a30c6c370a57b8058ad9153973e0e60
Kateryna Lapshyna <kateryna.lapshyna.oi.2024@lpnu.ua>
2026-03-17 22:37:48 +0200

    вдруге для сервера

--- server.js
+++ server.js
@@ -84,6 +84,7 @@ app.get("/api/protected", verifyToken, (req, res) => {
 });
 
 // --- ЗАПУСК СЕРВЕРА ---
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
